9b883562ff20b2f0ba64e2a352d859a7745f1ead
Christian Gramling <gramlinc@uci.edu>
2018-05-23 17:56:46 -0700

    Item Spawners

---
+++ Garden Guardians/Assets/ItemSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject Item;
+
+	public gameManager gM;
+
+	private float time = 60f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.childCount == 0 && gM.timer <= time -10f)
+		{
+			Spawn();
+			time = gM.timer;
+		}
+		
+	}
+
+	void Spawn()
+	{
+		GameObject spawn = Instantiate(Item);
+        spawn.transform.position = this.transform.position;
+		spawn.transform.SetParent(this.transform);
+	}
+}
